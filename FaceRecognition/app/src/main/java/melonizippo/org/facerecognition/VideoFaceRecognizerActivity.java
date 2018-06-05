8b194bbc40ad5493cbf1b7d6d16a568bf2ea7ec9
rzippo <raffaele.zippo@live.it>
2018-06-05 22:35:25 +0200

    Add code for third-party record then analysis

--- FaceRecognition/app/src/main/java/melonizippo/org/facerecognition/VideoFaceRecognizerActivity.java
+++ FaceRecognition/app/src/main/java/melonizippo/org/facerecognition/VideoFaceRecognizerActivity.java
@@ -1,5 +1,8 @@
 package melonizippo.org.facerecognition;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,7 +25,6 @@ public class VideoFaceRecognizerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_video_face_recognizer);
     }
 
-
     public void buttonStart(View view) {
         ImageView frameView = findViewById(R.id.frameView);
 
@@ -32,12 +34,37 @@ public class VideoFaceRecognizerActivity extends AppCompatActivity {
                     Parameters.STORAGE_FILE,
                     frameView );
 
-            videoFaceRecognizer.initVideo(
-                    new File(Parameters.VIDEO_PATH),
-                    Parameters.DEST_VIDEO );
-            videoFaceRecognizer.analyzeVideo();
-            videoFaceRecognizer.closeRecorder();
+            dispatchTakeVideoIntent();
         }
         catch (Exception e) {}
+    }
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+
+    private void dispatchTakeVideoIntent()
+    {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK)
+        {
+            Uri videoUri = intent.getData();
+            File videoFile = new File(videoUri.getPath());
+
+            try{
+                videoFaceRecognizer.initVideo(
+                        videoFile,
+                        null);
+                videoFaceRecognizer.analyzeVideo();
+                videoFaceRecognizer.closeRecorder();
+            }
+            catch (Exception e) {}
+        }
     }
 }
