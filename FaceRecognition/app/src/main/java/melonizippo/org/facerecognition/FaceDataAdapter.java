0d09ae70b6f36c1ec805a756f068ccf5b4c85656
rzippo <raffaele.zippo@live.it>
2018-06-30 16:34:46 +0200

    Cleanup unused field

--- FaceRecognition/app/src/main/java/melonizippo/org/facerecognition/FaceDataAdapter.java
+++ FaceRecognition/app/src/main/java/melonizippo/org/facerecognition/FaceDataAdapter.java
@@ -1,27 +1,23 @@
 package melonizippo.org.facerecognition;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import melonizippo.org.facerecognition.database.FaceData;
 
 public class FaceDataAdapter extends ArrayAdapter<FaceData>
 {
-    private List<FaceData> dataSet;
     Context mContext;
 
-    FaceDataAdapter(List<FaceData> data, Context context)
+    FaceDataAdapter(List<FaceData> dataSet, Context context)
     {
-        super(context, R.layout.face_preview, data);
-        this.dataSet = data;
+        super(context, R.layout.face_preview, dataSet);
         this.mContext=context;
     }
 
